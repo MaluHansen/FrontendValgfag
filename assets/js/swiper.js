999d36b10898c9e04679dec8f5117ced9fedbbae
Rasmus <rasmuspedersen14@gmail.com>
2024-09-20 00:09:25 +0200

    tilføjet swiper slider
    tilføjet vha js og css bibliotek

---
+++ assets/js/swiper.js
@@ -0,0 +1,37 @@
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    slidesPerGroup: 1,
+    loop: true,
+    autoplay: {
+      delay: 2500, 
+      disableOnInteraction: false, 
+      pauseOnMouseEnter: true, 
+      reverseDirection: false, 
+    },
+    speed: 1000,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  
+  var swiperWrapper = document.querySelector('.swiper-wrapper');
+  var slides = swiperWrapper.querySelectorAll('.swiper-slide');
+  
+  // Begrænser max antal cards til 10 på slideren
+  if (slides.length > 10) {
+    for (var i = 10; i < slides.length; i++) {
+      slides[i].remove();
+    }
+  }
+  
